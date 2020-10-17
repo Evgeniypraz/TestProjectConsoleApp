5c7f0e82e17cf0f77aae4bfd611a23f3cf56cbc2
evprazukin <evgeniypraz@gmail.com>
2020-10-17 22:56:27 +0300

    Add project files.

---
+++ TestProjectConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MS.TF.PublishTestResultsTask;
+
+namespace TestProjectConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
